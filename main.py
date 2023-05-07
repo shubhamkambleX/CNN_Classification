52bbb6c321c5bbb279588f9539099e8ad3b102d1
shubhamkambleX <196shubham@gmail.com>
2023-05-07 16:50:01 +0530

    prepare base model pipline created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cnnClassifier.pipline.stage_01_data_ingestion import DataIngestionTrainingPipline
+from cnnClassifier.pipline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipline
 from cnnClassifier import logger
 
 
@@ -8,6 +9,18 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} Started <<<<<<")
     data_ingestion = DataIngestionTrainingPipline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n================x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} Started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipline()
+    prepare_base_model.main()
     logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n================x")
 except Exception as e:
     logger.exception(e)
